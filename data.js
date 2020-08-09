668c1a822a432ebef1d7d0d01f7f06157c2e8384
Ho <rbish005@BKUSL-1FP6P3XY.local>
2020-08-09 18:41:44 -0400

    attempt 2

--- data.js
+++ data.js
@@ -1,33 +1,39 @@
 let responses = [];
 const userResponsesSection = document.querySelector('#user-responses');
 
-
 const fetchUserResponses = async () => {
   const response = await fetch(
-    'http://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/e/2PACX-1vRlfsW_69UVHfzhWeunjtt-c1TmEi_dRrTNtMoCNK3l0fR-3oSAs2o8BjsGssfQHIjp04orLj3IprA5/pub?output=csv'
-    );
+    'https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/e/2PACX-1vRlfsW_69UVHfzhWeunjtt-c1TmEi_dRrTNtMoCNK3l0fR-3oSAs2o8BjsGssfQHIjp04orLj3IprA5/pub?output=csv'
+  );
   const data = await response.text();
-  const results = Papa.parse(data, {header: true});
+  const results = Papa.parse(data, { header: true });
   responses = results.data;
-  
 };
 
 const renderUserResponse = userResponse => {
-  const name = userResponse["What is your name? "];
-  const restaurantName = userResponse["Name one of your favorite restaurants in the Miami area."];
-  const restaurantWebsite = userResponse["Please provide a link to the restaurant's website."];
-  const love = userResponse["Why do you love this restaurant? "];
-  const cuisine = userResponse["What type of cuisine does the restaurant serve?"];
-  const greatFor = userResponse["This restaurant is great for (check all that apply):"];
-  const mustTry = userResponse["Tell us your favorite \"must try\" item on the menu."];
-  const restaurantPic = userResponse["Upload a picture of the restaurant or the food you ate."];
+  const name = userResponse['What is your name? '];
+  const restaurantName =
+    userResponse['Name one of your favorite restaurants in the Miami area.'];
+  const restaurantWebsite =
+    userResponse["Please provide a link to the restaurant's website."];
+  const love = userResponse['Why do you love this restaurant? '];
+  const cuisine =
+    userResponse['What type of cuisine does the restaurant serve?'];
+  const greatFor =
+    userResponse['This restaurant is great for (check all that apply):'];
+  const mustTry =
+    userResponse['Tell us your favorite "must try" item on the menu.'];
+  const restaurantPic =
+    userResponse['Upload a picture of the restaurant or the food you ate.'];
   const googlePhotoId = restaurantPic.split('id=')[1];
   const greatForListItems = greatFor.split(', ');
   console.log(greatForListItems);
-  const greatForListArray = greatForListItems.map( item => {
-    return `
+  const greatForListArray = greatForListItems
+    .map(
+      item => `
       <li>&#10003${item}</li>`
-  }).join("")  
+    )
+    .join('');
   return `
     <div class = "user-response"> 
       <a href="${restaurantWebsite}">
@@ -65,15 +71,14 @@ const renderUserResponse = userResponse => {
 
         
     </div>
-  `; 
-  };
-
+  `;
+};
 
 const FetchAndShowResponses = async () => {
   await fetchUserResponses();
   const eachUserResponseHTML = responses.map(renderUserResponse);
-  const allUserResponsesHTML = eachUserResponseHTML.join("");
-  userResponsesSection.innerHTML = allUserResponsesHTML; 
-}
+  const allUserResponsesHTML = eachUserResponseHTML.join('');
+  userResponsesSection.innerHTML = allUserResponsesHTML;
+};
 
 FetchAndShowResponses();
